43e3dae0af916f7e319d7c7e471beefe682e4b59
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2022-01-18 10:17:52 +0900

    refactor: TodoCollection에서 Map사용

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,10 @@ const id = myTodoCollection.addTodo("친구 만나기");
 myTodoCollection.markComplete(id, true);
 
 console.log(`${myTodoCollection.userName}`);
-myTodoCollection.todoItems.forEach((item) => item.printDetails());
+
+myTodoCollection.getTodoItems(true).forEach((item) => item.printDetails());
+console.log("=========================");
+myTodoCollection.getTodoItems(false).forEach((item) => item.printDetails());
+console.log("=========================");
+myTodoCollection.removeComplete();
+myTodoCollection.getTodoItems(true).forEach((item) => item.printDetails());
