9a0a8fc6dc090efdf92500ec6e65025930da2b00
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2022-03-07 18:18:08 +0900

    add internal mock example

---
+++ jest/src/messageService.js
@@ -0,0 +1,17 @@
+module.exports = {
+  sendEmail(email, message) {
+    /* 이메일 보내는 코드 */
+  },
+
+  sendSMS(phone, message) {
+    /* 문자를 보내는 코드 */
+  },
+};
+
+// export const sendEmail = function (email, message) {
+//   /* 이메일 보내는 코드 */
+// };
+
+// export const sendSMS = function (phone, message) {
+//   /* 문자를 보내는 코드 */
+// };
---
+++ jest/src/userRegistService.js
@@ -0,0 +1,15 @@
+const messageService = require("./messageService");
+
+exports.register = function register(user) {
+  /* DB에 회원 추가 */
+  const message = "회원 가입을 환영합니다!";
+  messageService.sendEmail(user.email, message);
+  messageService.sendSMS(user.phone, message);
+};
+
+exports.deregister = function deregister(user) {
+  /* DB에 회원 삭제 */
+  const message = "탈퇴 처리 되었습니다.";
+  messageService.sendEmail(user.email, message);
+  messageService.sendSMS(user.phone, message);
+};
