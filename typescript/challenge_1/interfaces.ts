1625e22123b138a3176b6632157948f7998c577c
vmfhrmfoaj <vmfhrmfoaj@nate.com>
2022-07-07 21:30:52 +0900

    ~challenge_1

--- typescript/challenge_1/interfaces.ts
+++ typescript/challenge_1/interfaces.ts
@@ -1,26 +1,31 @@
-[
+interface House {
+  name: string;
+  planets: string | string[];
+}
+
+interface HouseWithID extends House {
+  id: number;
+}
+
+type FilterFn = (house: House) => boolean;
+
+function findHouses(houses: string): HouseWithID[];
+function findHouses(houses: string, filter: FilterFn): HouseWithID[];
+function findHouses(houses: House[]): HouseWithID[];
+function findHouses(houses: House[], filter: FilterFn): HouseWithID[];
+function findHouses(input: string | House[], filter?: FilterFn): HouseWithID[] {
+  const houses: House[] = typeof input === "string" ? JSON.parse(input) : input;
+  return (filter ? houses.filter(filter) : houses).map((house) => ({
+    id: houses.indexOf(house),
+    ...house,
+  }));
+}
+
+const houses = [
   { name: "Atreides", planets: "Calladan" },
   { name: "Corrino", planets: ["Kaitan", "Salusa Secundus"] },
   { name: "Harkonnen", planets: ["Giedi Prime", "Arrakis"] },
 ];
-interface House {
-  // ...
-}
-
-interface HouseWithID {
-  // ...
-}
-
-function findHouses(houses: string): HouseWithID[];
-function findHouses(
-  houses: string,
-  filter: (house: House) => boolean
-): HouseWithID[];
-function findHouses(houses: House[]): HouseWithID[];
-function findHouses(
-  houses: House[],
-  filter: (house: House) => boolean
-): HouseWithID[];
 
 console.log(
   findHouses(JSON.stringify(houses), ({ name }) => name === "Atreides")
